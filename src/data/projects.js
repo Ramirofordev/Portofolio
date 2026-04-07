635ec20df2a343d74d40021a5a9e04962c7a2a53
RamiroforDev <joseignacioramirocastro22@gmail.com>
2026-04-07 23:06:31 +0200

    Changes in the project description

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [{
     img: "/Portofolio/assets/demo-3.gif",
     title: "Proyecto de Agile",
     technologies: [{name: "Python", icon: "/Portofolio/assets/python.svg", color: "rgba(75, 139, 190, 0.2)"}, {name: "Flask", icon: "/Portofolio/assets/flask.svg", color: "gray"}, {name: "HTML", icon: "/Portofolio/assets/html5.svg", color: "rgba(240, 101, 41, 1)"}, {name: "JavaScript", icon: "/Portofolio/assets/javascript.svg", color: "rgba(240, 219, 79, 1)"}, {name: "CSS", icon: "/Portofolio/assets/css.svg", color: "rgba(38, 77, 228, 1)"}],
-    description: "Aplicación enfocada en la gestión de tareas y productividad, combinando un sistema de todo-list con la técnica Pomodoro. Desarrollada siguiendo principios Agile, permitiendo organizar tareas, priorizarlas y mejorar la gestión del tiempo mediante ciclos de trabajo y descanso.",
+    description: "Aplicación web de gestión tareas orientada proyectos y contextos, incorporando funcionalidades como CRUD completo, filtrado avanzado y visualización adpatada a dispositivos móviles. El sistema permite organizar tareas mediante proyectos y contextos, incorporando funcionalidades como CRUD completo, filtrado avanzado y visualización adaptada a dispositivos móviles. A nivel técnico, el proyecto esta estructurado en capas (dominio, servicios, repositorios), aplicando principios SOLID y separación de responsabilidades, lo que facilita su escalabilidad y mantenimiento.",
     links: {
         demo: "https://agile-project-o2pv.onrender.com",
         repo: "https://github.com/Ramirofordev/Agile-Project"    
